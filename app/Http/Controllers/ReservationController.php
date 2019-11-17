1e53dd76feaba855f08213b44efff9dc4df242d1
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-11-17 22:46:15 +0200

    Created controller skeletons

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+
 }
--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -8,5 +8,5 @@ use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
 {
-    
+
 }
